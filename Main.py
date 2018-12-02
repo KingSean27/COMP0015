f298acdd924d3ade4920a3b46c91d4495120b9ae
KingSean27 <44440591+KingSean27@users.noreply.github.com>
2018-12-02 22:18:44 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
-# 00000000000000000000000000000000000000000000000000000000000000000000000000079
- 
+## 
+#  This application is a fair grade allocator based on the work by: 
+#  A. Procaccia, J. Goldman, N. Shah and D. Kurokawa.
+#  Author: Group X
+#  Date: Novemeber 2018
+
 
 # imports modules used
 import time
@@ -10,7 +14,11 @@ import sic
 
 # all definitions
 def menu_screen():
-    """This function prints the menu options."""
+    """This function prints the menu options.
+    Note that it is only visual, so when called
+    later it just reminds the user of possible 
+    choices, it's not a recursive call"""
+    
     os.system('clear')
     print("Welcome to Split-it!")
 
@@ -22,17 +30,19 @@ def menu_screen():
     Quit           (Q)
     ''')
 
+    
 def menu_redirect():
     """This function redirects the user to a different page.
     The user provides an input and is then redirected
     to the corresponding page. If an invalid choice is entered
-    then the user is prompted to make another choice."""
+    then the user is prompted to make another choice.
+    This function is only called once!"""
 
     app_open = "*"
-  
     while app_open != "q": 
         global menu_choice
-        menu_choice = str(input("Please choose an option and press <Enter>: "))
+        menu_choice = str(input("Please choose an " 
+                                "option and press <Enter>: "))
         menu_choice = menu_choice.upper()
 
         if menu_choice == "A":
@@ -56,14 +66,23 @@ def menu_redirect():
             
             
 def menu():
+    """This starts off the programme
+    by calling the visual display and the 
+    actual loop which requires input"""
+    
     global project_dict
     project_dict = {}
     menu_screen()
     menu_redirect()
 
-###############  ################################################  ####################  ###############
+
 def enter_votes():
-    """This looks up the project and lets members enter votes"""
+    """This looks up the project entered
+    then prints the number of members 
+    attribute of the project. Then it 
+    allows team members to enter votes 
+    for each other with validation
+    constraints to stop bad input"""
 
     os.system('clear')
     global menu_choice
@@ -75,55 +94,50 @@ def enter_votes():
     if lookup in project_dict:
         number = project_dict[lookup].NoOfMems
         proname = project_dict[lookup].name
-        print("There are {} team members in {}".format(number, proname))
+        print("There are {} team members in {}"
+              .format(number, proname))
         
         i = 0
-
-        
         while i < number:
             votecount = 0 
             votedict = {}
             askvote1 = project_dict[lookup].members[i].name
-            print ("Enter {}'s points the number must add up to 100".format(askvote1))
+            print ("Enter {}'s points the number must add up to 100"
+                   .format(askvote1))
               
             for item in project_dict[lookup].members:
                 askvote2 = str(item.name)
                 if askvote1 == askvote2:
                     continue 
                 else:
-                    vote = input("\tEnter {}'s points for {} ".format(askvote1, askvote2))
+                    vote = input("\tEnter {}'s points for {} "
+                                 .format(askvote1, askvote2))
                     vote = vf.verif2.voteInput(vote)
                     votecount += vote 
                     votedict[askvote2] = vote 
             
             if votecount != 100:
-                print ("Sorry the votes you have entered do not add up to 100")
+                print ("Sorry the votes you have"
+                       "entered do not add up to 100")
             else: 
                 project_dict[lookup].members[i].votes.update (votedict)
-
                 i += 1
                 
-                 
-      
-            
         else: 
-            print ("All votes have been successfully entered")
-          
-        input("\n\nPress the any key to return to main menu.")  
-        
-        
-        
+            print ("All votes have been successfully entered")  
+             
     else:
         print ("This project does not exist in the database")
         
-    
-    
-    time.sleep(5)
+    input("\n\nPress any key followed by "
+          "<Enter> to return to the main menu.")
+    time.sleep(2)
     menu_screen()
-###########   ####################################################  ##################  ###############
+
 
 def show_projects():
-    """This displays a message for two seconds then clears the console."""
+    """This displays a message for 
+    two seconds then clears the console."""
     
     os.system('clear')
     global menu_choice
@@ -134,14 +148,16 @@ def show_projects():
 
 
 def about():
-    """This displays information about the programme and returns
-    to the menu screen on the user's command """
+    """This displays information about the 
+    programme and returns to the menu 
+    screen on the user's command """
 
     os.system('clear')
     global menu_choice
     menu_choice = ""
     print("This is Split-it a coursework marking app")
-    input("\n\nPress any key followed by <Enter> to return to the main menu.")
+    input("\n\nPress any key followed by "
+          "<Enter> to return to the main menu.")
     menu_screen()
     
 
@@ -158,15 +174,18 @@ def create_project():
 
     global project_dict
     project_dict = {}
-    project_dict[projectName] = sic.Project(theName=projectName, theNoOfMems=teamSize, theMembers=members)
+    project_dict[projectName] = sic.Project(theName=projectName, 
+                                            theNoOfMems=teamSize, 
+                                            theMembers=members)
 
   
-    input("\n\nPress the any key to return to main menu.")
+    input("\n\nPress any key followed by "
+          "<Enter> to return to the main menu.")
     menu_screen()
     
 
 
-#Call the main function to begin the programme
+#Calls the menu function which begins the programme
 menu()
 
-# 00000000000000000000000000000000000000000000000000000000000000000000000000079
+
